b045199ee4d8b185afb17264c1d77d4b15d75b08
dakshinamurthy <dakshinamurthy13@gmail.com>
2021-03-19 11:33:45 +0530

    Polynomial addition and subtraction

--- polynomialAddition/main.c
+++ polynomialAddition/main.c
@@ -1,164 +1,225 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 struct node
 {
-    int coff;
-    int pow;
+    int num;
+    int coeff;
     struct node *next;
-    struct node *prev;
 };
-struct node *head = NULL, *head1 = NULL, *head2 = NULL, *last1 = NULL, *last2 = NULL;
-struct node *list = NULL;
-struct node *current = NULL;
-void createlist1()
+struct node *start1 = NULL;
+struct node *start2 = NULL;
+struct node *start3 = NULL;
+struct node *start4 = NULL;
+struct node *last3 = NULL;
+struct node *create_poly(struct node *);
+struct node *display_poly(struct node *);
+struct node *add_poly(struct node *, struct node *, struct node *);
+struct node *sub_poly(struct node *, struct node *, struct node *);
+struct node *add_node(struct node *, int, int);
+int main()
 {
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the Coff\n");
-    scanf("%d", &newnode->coff);
-    printf("Enter the power\n");
-    scanf("%d", &newnode->pow);
-    newnode->next = NULL;
-    newnode->prev = NULL;
-    if (head1 == NULL){
-        head1 = newnode;
-        last1 = newnode;
-    }
-    else{
-        last1->next = newnode;
-        newnode->prev = last1;
-        last1 = newnode;
-    }
-}
-void createlist2()
-{
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the coff and power\n");
-    scanf("%d%d", &newnode->coff, &newnode->pow);
-    newnode->next = NULL;
-    newnode->prev = NULL;
-    if (head2 == NULL)
+    int option;
+    do
     {
-        head2 = newnode;
-        last2 = newnode;
+        printf("\n******* MAIN MENU *******");
+        printf("\n 1. Enter the first polynomial");
+        printf("\n 2. Display the first polynomial");
+        printf("\n 3. Enter the second polynomial");
+        printf("\n 4. Display the second polynomial");
+        printf("\n 5. Add the polynomials");
+        printf("\n 6. Display the result");
+        printf("\n 7. Subtract the polynomials");
+        printf("\n 8. Display the result");
+        printf("\n 9. EXIT");
+        printf("\n\n Enter your option : ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            start1 = create_poly(start1);
+            break;
+        case 2:
+            start1 = display_poly(start1);
+            break;
+        case 3:
+            start2 = create_poly(start2);
+            break;
+        case 4:
+            start2 = display_poly(start2);
+            break;
+        case 5:
+            start3 = add_poly(start1, start2, start3);
+            break;
+        case 6:
+            start3 = display_poly(start3);
+            break;
+        case 7:
+            start4 = sub_poly(start1, start2, start4);
+            break;
+        case 8:
+            start4 = display_poly(start4);
+            break;
+        }
+    } while (option != 9);
+    return 0;
+}
+struct node *create_poly(struct node *start)
+{
+    struct node *new_node, *ptr;
+    int n, c;
+    printf("\n Enter the number : ");
+    scanf("%d", &n);
+    printf("\t Enter its coefficient : ");
+    scanf("%d", &c);
+    while (n != -1)
+    {
+        if (start == NULL)
+        {
+            new_node = (struct node *)malloc(sizeof(struct node));
+            new_node->num = n;
+            new_node->coeff = c;
+            new_node->next = NULL;
+            start = new_node;
+        }
+        else
+        {
+            ptr = start;
+            while (ptr->next != NULL)
+                ptr = ptr->next;
+            new_node = (struct node *)malloc(sizeof(struct node));
+            new_node->num = n;
+            new_node->coeff = c;
+            new_node->next = NULL;
+            ptr->next = new_node;
+        }
+        printf("\n Enter the number : ");
+        scanf("%d", &n);
+        if (n == -1)
+            break;
+        printf("\t Enter its coefficient : ");
+        scanf("%d", &c);
+    }
+    return start;
+}
+struct node *display_poly(struct node *start)
+{
+    struct node *ptr;
+    ptr = start;
+    while (ptr != NULL)
+    {
+        printf("\n%d x %d\t", ptr->num, ptr->coeff);
+        ptr = ptr->next;
+    }
+    return start;
+}
+struct node *add_poly(struct node *start1, struct node *start2, struct node *start3)
+{
+    struct node *ptr1, *ptr2;
+    int sum_num, c;
+    ptr1 = start1,
+    ptr2 = start2;
+    while (ptr1 != NULL && ptr2 != NULL)
+    {
+        if (ptr1->coeff == ptr2->coeff)
+        {
+            sum_num = ptr1->num + ptr2->num;
+            start3 = add_node(start3, sum_num, ptr1->coeff);
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        else if (ptr1->coeff > ptr2->coeff)
+        {
+            start3 = add_node(start3, ptr1->num, ptr1->coeff);
+            ptr1 = ptr1->next;
+        }
+        else if (ptr1->coeff < ptr2->coeff)
+        {
+            start3 = add_node(start3, ptr2->num, ptr2->coeff);
+            ptr2 = ptr2->next;
+        }
+    }
+    if (ptr1 == NULL)
+    {
+        while (ptr2 != NULL)
+        {
+            start3 = add_node(start3, ptr2->num, ptr2->coeff);
+            ptr2 = ptr2->next;
+        }
+    }
+    if (ptr2 == NULL)
+    {
+        while (ptr1 != NULL)
+        {
+            start3 = add_node(start3, ptr1->num, ptr1->coeff);
+            ptr1 = ptr1->next;
+        }
+    }
+    return start3;
+}
+struct node *sub_poly(struct node *start1, struct node *start2, struct node *start4)
+{
+    struct node *ptr1, *ptr2;
+    int sub_num, c;
+    ptr1 = start1, ptr2 = start2;
+    do
+    {
+        if (ptr1->coeff == ptr2->coeff)
+        {
+            sub_num = ptr1->num - ptr2->num;
+            start4 = add_node(start4, sub_num, ptr1->coeff);
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        else if (ptr1->coeff > ptr2->coeff)
+        {
+            start4 = add_node(start4, ptr1->num, ptr1->coeff);
+            ptr1 = ptr1->next;
+        }
+        else if (ptr1->coeff < ptr2->coeff)
+            {
+                start4 = add_node(start4, ptr2->num, ptr2->coeff);
+                ptr2 = ptr2->next;
+            }
+    } while (ptr1 != NULL || ptr2 != NULL);
+    if (ptr1 == NULL)
+    {
+        while (ptr2 != NULL)
+        {
+            start4 = add_node(start4, ptr2->num, ptr2->coeff);
+            ptr2 = ptr2->next;
+        }
+    }
+    if (ptr2 == NULL)
+    {
+        while (ptr1 != NULL)
+        {
+            start4 = add_node(start4, ptr1->num, ptr1->coeff);
+            ptr1 = ptr1->next;
+        }
+    }
+    return start4;
+}
+struct node *add_node(struct node *start, int n, int c)
+{
+    struct node *ptr, *new_node;
+    if (start == NULL)
+    {
+        new_node = (struct node *)malloc(sizeof(struct node));
+        new_node->num = n;
+        new_node->coeff = c;
+        new_node->next = NULL;
+        start = new_node;
     }
     else
     {
-        last2->next = newnode;
-        newnode->prev = last2;
-        last2 = newnode;
+        ptr = start;
+        while (ptr->next != NULL)
+            ptr = ptr->next;
+        new_node = (struct node *)malloc(sizeof(struct node));
+        new_node->num = n;
+        new_node->coeff = c;
+        new_node->next = NULL;
+        ptr->next = new_node;
     }
-}
-void display()
-{
-    struct node *temp;
-    temp = list;
-    while (temp != NULL)
-    {
-        printf("%s%dx^%d", (temp->coff > 0 ? "+" : "-"), temp->coff, temp->pow);
-        temp = temp->next;
-    }
-}
-
-void poly(){
-    struct node *temp1, *temp2;
-    struct node *newnode;
-    int sum = 0;
-    temp1 = head1;
-    temp2 = head2;
-    while (temp1 != NULL && temp2 != NULL){
-        newnode = (struct node *)malloc(sizeof(struct node));
-        newnode->next = NULL;
-        newnode->prev = NULL;
-        if (list == NULL){
-            list = newnode;
-            current = newnode;
-        }
-        else{
-            current->next = newnode;
-            newnode->prev = current;
-            current = newnode;
-            if (temp1->pow == temp2->pow){
-                sum = temp1->coff + temp2->coff;
-                newnode->coff = sum;
-                newnode->pow = temp1->pow;
-                temp1 = temp1->next;
-                temp2 = temp2->next;
-            }
-            else if (temp1->pow > temp2->pow){
-                newnode->coff = temp1->coff;
-                newnode->pow = temp1->pow;
-                temp1 = temp1->next;
-            }
-            else{
-                newnode->coff = temp2->coff;
-                newnode->pow = temp2->pow;
-                temp2 = temp2->next;
-            }
-        }
-        if (temp1 == NULL){
-            while (temp2 != NULL){
-                newnode = (struct node *)malloc(sizeof(struct node));
-                newnode->coff = temp2->coff;
-                newnode->pow = temp2->pow;
-                newnode->next = NULL;
-                newnode->prev = NULL;
-                if (list == NULL)
-                    list = newnode;
-                else
-                    current->next = newnode;
-                newnode->prev = current;
-                current = newnode;
-                temp2 = temp2->next;
-            }
-        }
-        else{
-            while (temp1 != NULL){
-                newnode = (struct node *)malloc(sizeof(struct node));
-                newnode->coff = temp1->coff;
-                newnode->pow = temp1->pow;
-                newnode->next = NULL;
-                newnode->prev = NULL;
-                if (list == NULL)
-                    list = newnode;
-                else
-                    current->next = newnode;
-                newnode->prev = current;
-                current = newnode;
-                temp1 = temp1->next;
-            }
-        }
-    }
-}
-int main(){
-    int choice;
-    while (1)
-    {
-        printf("1.create Polynomial expression1 \n");
-        printf("2.create Polynomial expression 2 \n");
-        printf("3.Polynomial Addition\n");
-        printf("4.display\n");
-        printf("5. exit\n");
-        printf("Enter your choice\n");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1:
-            createlist1();
-            break;
-        case 2:
-            createlist2();
-            break;
-        case 3:
-            poly();
-            break;
-        case 4:
-            display();
-            break;
-        case 5:
-            exit(0);
-        }
-    }
-    return 0;
+    return start;
 }

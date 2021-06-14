e2c031b9072daf5e2dfba34eb6c37cbd0e98f26b
dakshinamurthy <dakshinamurthy13@gmail.com>
2021-06-14 12:42:37 +0530

    Selection Sort

--- selectionSort/main.c
+++ selectionSort/main.c
@@ -1,29 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
+int main()
+{
+   int i, j, count, min, temp;
+   float start, end;
 
-int main(){
-   int i, j, count, temp;
-   float start,end;
-
-   printf("How many numbers u are going to enter?: ");
-   scanf("%d",&count);
+   printf("How many numbers u are going to enter?: \n");
+   scanf("%d", &count);
    int number[count];
 
-   for(i=0;i<count;i++)
-        number[i]=rand();
-    start=clock();
-   for(i=0;i<count;i++){
-      for(j=i+1;j<count;j++){
-         if(number[i]>number[j]){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
+   //printf("Enter the elements of the array\n");
+   //for(i=0;i<count;i++){
+   //     scanf("%d",&number[i]);
+   //}
+
+   for (i = 0; i < count; i++)
+      number[i] = rand();
+   printf("Unsorted array\n");
+   for (i = 0; i < count; i++)
+   {
+      printf("%d\t", number[i]);
+   }
+   printf("\n");
+   start = clock();
+   for (i = 0; i < count - 1; i++)
+   {
+      min = i;
+      for (j = i + 1; j < count; j++)
+      {
+         if (number[min] > number[j])
+         {
+            min = j;
          }
       }
+      temp = number[min];
+      number[min] = number[i];
+      number[i] = temp;
    }
-    end=clock();
-    printf("\n Time taken to sort %d numbers is %f Secs",count, (((double)(end-start))/CLOCKS_PER_SEC));
+   end = clock();
+   printf("Sorted array\n");
+   for (i = 0; i < count; i++)
+   {
+      printf("%d\t", number[i]);
+   }
+   printf("\n");
+   printf("\n Time taken to sort %d numbers is %f Secs", count, (((double)(end - start)) / CLOCKS_PER_SEC));
    return 0;
 }

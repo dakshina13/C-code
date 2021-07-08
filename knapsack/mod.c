f8b2cbdc08c472102a0c83c1b88760e66a22bee2
dakshinamurthy <dakshinamurthy13@gmail.com>
2021-07-08 11:46:59 +0530

    0/1 Knapsack modification

--- knapsack/mod.c
+++ knapsack/mod.c
@@ -28,34 +28,41 @@ void knapsack(int w[], int v[], int s, int n)
     res = k[n][s];
     printf("\n\nMaximum Value that can be obtained is : %d", res);
     j = s;
-    printf("\nAnd the objects with their respective Weights selected are :");
+    printf("\nSelected objects are :  ");
     for (i = n; i > 0 && res > 0; i--)
     {
         if (res == k[i - 1][j])
             continue;
         else
         {
-            printf("%d\t", w[i - 1]);
+            printf("%d\t", i);
             res = res - v[i - 1];
             j = j - w[i - 1];
         }
         count++;
     }
-    printf("\n\n the count of the items selected: %d:", count);
+    printf("\n\nThe count of the items selected: %d", count);
 }
 
 int main()
 {
-    int w[10], v[10], s, n, i;
-    printf("\nEnter the Number of objects : ");
-    scanf("%d", &n);
-    printf("\nEnter the Weights of the objects : ");
-    for (i = 0; i < n; i++)
-        scanf("%d", &w[i]);
-    printf("\nEnter the Values of the objects : ");
-    for (i = 0; i < n; i++)
-        scanf("%d", &v[i]);
-    printf("\nEnter the Size of the KnapSack : ");
-    scanf("%d", &s);
-    knapsack(w, v, s, n);
+    int valsize, i, wtsize, wtkey;
+    printf("enter the size of value and weight arrays\n");
+    scanf("%d %d", &valsize, &wtsize);
+    int val[valsize];
+    int wt[wtsize];
+    printf("enter the values array\n");
+    for (i = 0; i < valsize; i++)
+    {
+        scanf("%d", &val[i]);
+    }
+    printf("enter the weight array\n");
+    for (i = 0; i < wtsize; i++)
+    {
+        scanf("%d", &wt[i]);
+    }
+    printf("enter the weight key\n");
+    scanf("%d", &wtkey);
+    knapsack(wt, val, wtkey, valsize);
+    return 0;
 }
